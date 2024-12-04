3dfd84ce3de1215f73fecf11aca87d0b1e51ba7e
shahidkkn <shahidkkn@gmail.com>
2024-12-04 10:09:43 +0530

    initial commit

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region     = "ap-south-1"
+}
+
+
+resource "aws_instance" "ourfirst" {
+  ami           = "ami-0447a12f28fddb066"
+  instance_type = "t2.small"
+#  tags = {
+#    Name = "sentinel-server"
+#  }
+  
+}
